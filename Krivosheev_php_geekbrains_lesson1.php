21706195043280a952f38502256891c740d85661
vkb1109 <vkb1109@mail.ru>
2019-02-22 15:31:52 +0300

    Объявил переменные. Присвоил им значения. Определил функцию текущего года. Вывел title и h1 с помощью PHP.

--- Krivosheev_php_geekbrains_lesson1.php
+++ Krivosheev_php_geekbrains_lesson1.php
@@ -1,14 +1,24 @@
+<?php
+    $titleSite = "Год текущий";
+    $yearNow = date ('Y');
+    $headSite = "Сегодня $yearNow год на дворе.";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    
+    <?php
+        echo "<title>$titleSite</title>"
+    ?>
+
 </head>
 <body>
     <?php
-        
+        echo "<h1>$headSite</h1>"    
     ?>
 </body>
 </html>
