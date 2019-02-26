d86b65d04ef6e3a01d2cc7cb75b38a20ad45c0f7
vkb1109 <vkb1109@mail.ru>
2019-02-26 15:49:44 +0300

    Убрал лишнее из файла.

--- Krivosheev_php_geekbrains_lesson2.php
+++ Krivosheev_php_geekbrains_lesson2.php
@@ -3,6 +3,8 @@
 $a = 6;
 $b = 3;
 
+function double_br() {echo "<br><hr><br>";}
+
 function sum ($x, $y){
     return $x + $y;
 }
@@ -119,75 +121,6 @@ function power($val, $pow){
         echo "<h1>Задание 7.</h1>";
     ?>
 
-    <?php
-        $x = 42;
-        $y = 5;
-        $now = "night"; 
-        $max = ($x > $y) ? $x : $y;
-        $avg = average($x, $y);
-        
-        function compare_numbers ($x, $y){
-        if ($x > $y)
-            echo '$x больше $y';
-        else if ($x < $y)
-            echo '$x меньше $y';
-        else
-            echo '$x равен $y';
-        }
-        
-        function double_br() {echo "<br><hr><br>";}
-
-        function average($x, $y){
-            return ($x + $y)/2;
-        }
-
-        function mult($a, $b = 1){
-            return $a * $b;
-        }
-        function fibonacci($n, $prev1 = 1, $prev2 = 0){
-            $current = $prev1 + $prev2;
-            echo "$current";
-            if ($n > 1) 
-                fibonacci($n - 1, $current, $prev1);
-        }
-        
-
-        compare_numbers($x, $y);
-
-        double_br();
-
-        switch ($now){
-            case "night":
-                echo "Доброй ночи!";
-                break;
-            case "morning":
-                echo "Доброе утро!";
-                break;
-            case "evening":
-                echo "Добрый вечер!";
-                break;
-            default:
-                echo "Добрый день!";
-                break;
-        }
-        
-        double_br();
-                
-        echo $max;
-        //echo 'Здесь должно выводиться значение переменной $max';
-
-        double_br();
-
-        echo $avg;
-
-        double_br();
-
-        echo mult(8, 2);
-        
-        double_br();
-
-        fibonacci(15);
-    ?>
 
 </body>
 </html>
