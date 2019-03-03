f80ee26b5b78d2270711bfe3a0ef135c5d2801ff
vkb1109 <vkb1109@mail.ru>
2019-03-03 13:52:48 +0300

    Выполнил задание 1.

---
+++ Krivosheev_php_geekbrains_lesson3.php
@@ -0,0 +1,62 @@
+<?php
+    $i = 0;
+    $n = 100;
+
+    function double_br() {echo "<br><hr><br>";}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Домашнее задание. Урок 3.</h1><br><hr><br>";
+        
+        echo "<h2>Задание 1.</h2>";
+        double_br();
+        while ($i <= $n) {
+            if ($i % 3 == 0) {
+                echo "$i<br>";
+                $i++;
+            }
+            else {
+                $i++;
+                continue;
+            }
+        }
+        
+        echo "<h2>Задание 2.</h2>";
+        double_br();
+
+    ?>
+    
+    
+    <?php // Тренировка по методичке и другим материалам.
+        /*
+        $n = 10;
+        $i = 1;
+        while (true) {
+            echo "$i<br>";
+            $i++;
+            if ($i > $n)
+                break;
+        }
+        
+        do{
+            echo "$i<br>";
+            $i++;
+        } while ($i <= $n);
+        
+        for ($i = 1; $i <= $n; $i++) {
+            if ($i % 2 == 0)
+                continue;
+            echo "$i<br>";
+        }
+        */
+    ?>
+</body>
+</html>
